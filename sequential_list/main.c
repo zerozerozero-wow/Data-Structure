7767ce2d3c517d51612e592c6d90246651f9f4b5
Jianghaipeng <775133979@qq.com>
2022-01-20 17:34:15 +0800

    2022_1_20

---
+++ sequential_list/main.c
@@ -0,0 +1,19 @@
+/**
+ * 线性表的顺序表示和实现
+ * @date 2022.1.20
+ */
+
+#include <stdio.h>
+
+
+/* 顺序表的存储结构 */
+#define MAXSIZE 100
+typedef struct {
+    ElemType *elem;     //存储空间的基地址
+    int length;         //当前长度
+} SqList;
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
